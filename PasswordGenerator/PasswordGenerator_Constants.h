df9671d816ef7017a752b9a8064a1f1bc31661e4
michalszubinski <mich999.pl@gmail.com>
2023-12-30 14:50:20 +0100

    Resolved another compiler error

--- PasswordGenerator/PasswordGenerator_Constants.h
+++ PasswordGenerator/PasswordGenerator_Constants.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "PasswordGenerator_Libraries.h"
 
-static struct PasswordGenerator_Constants {
+struct PasswordGenerator_Constants {
     // Default values for Settings
     // 
     // Default character group's characters
@@ -20,15 +20,3 @@ static struct PasswordGenerator_Constants {
     static const bool BOOL_cg_commonSymbols     ;
 
 };
-// DEFAULT VALUES FOR SETTINGS
-const std::string PasswordGenerator_Constants::STR_cg_lowerCharacters{ "abcdefghijklmnopqrstuvwxyz" };
-const std::string PasswordGenerator_Constants::STR_cg_upperCharacters{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-const std::string PasswordGenerator_Constants::STR_cg_numbers{ "0123456789" };
-const std::string PasswordGenerator_Constants::STR_cg_commonSymbols{ R"MSG( !"#$ % &'()*+,-./:;<=>?@[\]^_`{|}~")MSG" };
-
-const int INT_passwordLength = 8;
-
-static const bool BOOL_cg_lowerCharacters   = 1;
-static const bool BOOL_cg_upperCharacters   = 1;
-static const bool BOOL_cg_numbers           = 1;
-static const bool BOOL_cg_commonSymbols     = 1;
